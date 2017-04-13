82902baee047d2df83f891f87f561d62b0e061ff
hyeyoung <shinhy8101@naver.com>
2017-04-13 21:27:53 +0900

    <making static folder>

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var express = require('express');
-
+var path = require('path');
 var app = express();
 
-app.use(express.static(__dirname + '/public')); //static폴더 세팅
+app.use(express.static(path.join(__dirname, 'public'))); //static폴더 세팅
 
 
 app.listen(8000, function(){
